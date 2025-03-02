fc4337380db4ee98a8f1e5b29f5744bc163a0735
khosro35 <mh.khosrotabar75@gmail.com>
2025-03-02 16:27:47 +0330

    save

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,6 +1,16 @@
+import { useLocalSearchParams, usePathname } from "expo-router";
+import { useState } from "react";
 import { View, Text } from "react-native";
 
 const Search = () => {
+  const path = usePathname();
+  const params = useLocalSearchParams<{ query?: string }>();
+  const [search, setSearch] = useState(params.query);
+
+  const handleSearch = (text: string) => {
+    setSearch(text);
+  };
+
   return (
     <View>
       <Text>Search</Text>
